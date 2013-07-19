cec6ec89139e6116d82b7d13ad7fb26592ab1150
Maxim Oskolsky <mo@coalla.ru>
2013-07-19 10:14:08 +0400

    Mail settings

--- mail.php
+++ mail.php
@@ -1,13 +1,15 @@
 <?php
 
   $send = $_REQUEST['send'];
-  $to = 'info@oskolsky.com';
+  $to = 'skycatgame@gmail.com';
+  $cc = 'info@oskolsky.com';
 
   if (isset($send)) {
     $data = $_REQUEST['data'];
     include_once ('lib/mail.class.php');
     $mail = new mail('utf-8');
     $mail -> To($to);
+    $mail -> Cc($cc);
     $mail -> Subject('Feedback from the site sky-cat.info: "'.$data['subject'].'"');
     $mail -> Body('Name: '.$data['name']."\n".'E-mail: '.$data['email']."\n".'Inquiry: '.$data['inquiry']."\n".'Message: '.$data['message']);
     $mail -> Priority(3);
