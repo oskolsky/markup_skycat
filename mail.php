570ee0219f54383cb9af6dde2dd6d2ca22ba8e17
Maxim Oskolsky <mo@coalla.ru>
2013-07-24 15:23:29 +0400

    .

--- mail.php
+++ mail.php
@@ -8,8 +8,8 @@
     $data = $_REQUEST['data'];
     include_once ('lib/mail.class.php');
     $mail = new mail('utf-8');
-    $mail -> To($to);
-    $mail -> Cc($cc);
+    $mail -> To($cc);
+
     $mail -> Subject('Feedback from the site sky-cat.info: "'.$data['subject'].'"');
     $mail -> Body('Name: '.$data['name']."\n".'E-mail: '.$data['email']."\n".'Inquiry: '.$data['inquiry']."\n".'Message: '.$data['message']);
     $mail -> Priority(3);
